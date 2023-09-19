6e503adbbc9b88ba32abfbd030e5183f5fc7cfbf
shishankbhat <116367979+shishankbhat@users.noreply.github.com>
2023-09-20 00:12:36 +0530

    Create treequestion.cpp

---
+++ treequestion.cpp
@@ -0,0 +1,4 @@
+// https://practice.geeksforgeeks.org/problems/diameter-of-binary-tree/1
+https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
+https://practice.geeksforgeeks.org/problems/check-for-balanced-tree/1
+https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
