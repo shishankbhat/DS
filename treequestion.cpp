edf733da55de83883208727ea7562809fb989db0
shishankbhat <116367979+shishankbhat@users.noreply.github.com>
2023-09-21 01:42:37 +0530

    Update treequestion.cpp

--- treequestion.cpp
+++ treequestion.cpp
@@ -2,3 +2,6 @@
 https://practice.geeksforgeeks.org/problems/height-of-binary-tree/1
 https://practice.geeksforgeeks.org/problems/check-for-balanced-tree/1
 https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+https://practice.geeksforgeeks.org/problems/sum-tree/1
+https://practice.geeksforgeeks.org/problems/zigzag-tree-traversal/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+https://practice.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
