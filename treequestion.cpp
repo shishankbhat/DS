894a39908472c92b13bda2c976138847240339d0
shishankbhat <116367979+shishankbhat@users.noreply.github.com>
2023-09-22 00:43:23 +0530

    Update treequestion.cpp

--- treequestion.cpp
+++ treequestion.cpp
@@ -5,3 +5,7 @@ https://practice.geeksforgeeks.org/problems/determine-if-two-trees-are-identical
 https://practice.geeksforgeeks.org/problems/sum-tree/1
 https://practice.geeksforgeeks.org/problems/zigzag-tree-traversal/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
 https://practice.geeksforgeeks.org/problems/boundary-traversal-of-binary-tree/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article
+https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1
+https://practice.geeksforgeeks.org/problems/left-view-of-binary-tree/1
+https://practice.geeksforgeeks.org/problems/right-view-of-binary-tree/1
